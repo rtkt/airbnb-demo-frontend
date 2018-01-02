286105cdce87d31beaf30311144a0da094e57414
rtkt <yetanotherinbox@list.ru>
2018-01-03 00:48:44 +0700

    Added component 'Header'

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Header from '../Header/';
 
 const App = function () {
-  return (
-    <h1>test</h1>
-  );
+  return <Header />;
 };
 
 export default App;
---
+++ src/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styledComponents from 'styled-components';
+
+const Header = function () {
+  return (
+    <header className="container-fluid">
+      <h1>test</h1>
+    </header>
+  );
+};
+
+export default Header;
