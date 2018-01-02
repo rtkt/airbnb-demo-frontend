9c947d9c9640a37a275a73c1f541c7c3abf73e75
rtkt <yetanotherinbox@list.ru>
2018-01-03 02:57:50 +0700

    Successfully added logo element

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 import logo from './logo.svg';
 
-const Logo = styled.img`
+const height = 80;
 
+const Container = styled.header`
+  box-shadow: 0px 0.5px 0px rgba(72, 72, 72, 0.3);
+  height: ${height}px;
+  line-height: ${height}px;
+`;
+
+const Logo = styled.img`
+  display: inline-block;
+  vertical-align: middle;
 `;
 
 const Header = function () {
   return (
-    <header className="container-fluid">
-      <Logo className="col-lg-1" src={logo} alt="Logo" title="Logo" />
-    </header>
+    <Container className="container-fluid">
+      <div className="col-lg-1">
+        <Logo src={logo} alt="Logo" title="Logo" />
+      </div>
+    </Container>
   );
 };
 
