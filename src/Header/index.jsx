5844153caf311481c7d02d68a8767f21afa3eec2
rtkt <yetanotherinbox@list.ru>
2018-01-03 06:57:51 +0700

    Moved search field into a separate component

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Box } from '../common';
+import SearchField from '../SearchField/';
 import logo from './logo.svg';
-import searchIcon from './search.svg';
 
 const barHeight = 80;
-const searchHeight = 48;
 const links = {
   host: 'host',
   help: 'help',
@@ -26,26 +25,6 @@ const Container = styled.header`
 const Logo = styled.img`
   display: inline-block;
   vertical-align: middle;
-`;
-
-const SearchField = styled.input`
-  border: 1px solid rgba(72, 72, 72, 0.2);
-  border-radius: 4px;
-  box-sizing: border-box;
-  box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
-  height: ${searchHeight}px;
-  left: 0;
-  padding-left: 53px;
-  position: absolute;
-  top: 16px;
-  width: 100%;
-`;
-
-const SearchIcon = styled.img`
-  display: inline-block;
-  left: 16px;
-  position: absolute;
-  top: 29px;
 `;
 
 const Link = styled.a`
@@ -71,10 +50,7 @@ const Header = function () {
           <Logo src={logo} alt="Logo" title="Logo" />
         </div>
         <div className="col-lg-5">
-          <Box>
-            <SearchField type="text" name="location" placeholder="Try &quot;Miami&quot;" />
-            <SearchIcon src={searchIcon} alt="Search icon" title="Search icon" />
-          </Box>
+          <SearchField />
         </div>
         <div className="col-lg-4 col-lg-offset-2">
           <Box>
---
+++ src/SearchField/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Box } from '../common';
+import searchIcon from './search.svg';
+
+const searchHeight = 48;
+
+const Input = styled.input`
+  border: 1px solid rgba(72, 72, 72, 0.2);
+  border-radius: 4px;
+  box-sizing: border-box;
+  box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
+  height: ${searchHeight}px;
+  left: 0;
+  padding-left: 53px;
+  position: absolute;
+  top: 16px;
+  width: 100%;
+`;
+
+const Icon = styled.img`
+  display: inline-block;
+  left: 16px;
+  position: absolute;
+  top: 29px;
+`;
+
+const SearchField = function () {
+  return (
+    <Box>
+      <Input type="text" name="location" placeholder="Try &quot;Miami&quot;" />
+      <Icon src={searchIcon} alt="Search icon" title="Search icon" />
+    </Box>
+  );
+};
+
+export default SearchField;
