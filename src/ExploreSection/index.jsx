79e00ccc32f95c59a8f57da5cd182083e3803794
rtkt <yetanotherinbox@list.ru>
2018-01-03 15:53:49 +0700

    Created component HorizontalCard (and add these cards to the ExploreSection)

--- src/ExploreSection/index.jsx
+++ src/ExploreSection/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { H1 } from '../common';
+import HorizontalCard from '../HorizontalCard/';
+import photo1 from './photo-1.png';
+import photo2 from './photo-2.png';
+import photo3 from './photo-3.png';
 
 const Container = styled.div`
   margin-top: 128px;
@@ -11,6 +15,11 @@ const ExploreSection = function (props) {
   return (
     <Container className="container-fluid">
       <H1>{props.heading}</H1>
+      <div className="row">
+        <HorizontalCard img={photo1} text="Homes" />
+        <HorizontalCard img={photo2} text="Experiences" />
+        <HorizontalCard img={photo3} text="Restaurants" />
+      </div>
     </Container>
   );
 };
---
+++ src/HorizontalCard/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+  border: 1px solid rgba(72, 72, 72, 0.2);
+  border-radius: 4px;
+  box-sizing: border-box;
+  box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
+  height: 72px;
+  position: relative;
+`;
+
+const Text = styled.span`
+  font-family: 'Circular';
+  font-size: 17px;
+  left: 120px;
+  line-height: 72px;
+  position: absolute;
+  vertical-align: middle;
+`;
+
+const Img = styled.img`
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 96px;
+`;
+
+const HorizontalCard = function (props) {
+  return (
+    <div className="col-lg-4">
+      <Container>
+        <Img src={props.img} alt={props.text} title={props.text} />
+        <Text>{props.text}</Text>
+      </Container>
+    </div>
+  );
+};
+
+// HorizontalCard.defaultProps = {
+
+// };
+
+HorizontalCard.propTypes = {
+  img: PropTypes.string,
+  text: PropTypes.string,
+};
+
+export default HorizontalCard;
