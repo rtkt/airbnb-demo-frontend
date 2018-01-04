63eb9472a2b0d8ac6589eab843d2013f261b9f34
rtkt <yetanotherinbox@list.ru>
2018-01-04 16:50:37 +0700

    Created general MainSection component

---
+++ src/MainSection/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { H1 } from '../common';
+
+const Container = styled.section`
+  margin-top: ${props => props.marginTop}px;
+`;
+
+const MainSection = function (props) {
+  return (
+    <Container>
+      <H1>{props.heading}</H1>
+    </Container>
+  );
+};
+
+MainSection.defaultProps = {
+  marginTop: 0,
+};
+
+MainSection.propTypes = {
+  heading: PropTypes.string.isRequired,
+  /* eslint-disable react/no-unused-prop-types */
+  marginTop: PropTypes.number,
+  /* eslint-enable react/no-unused-prop-types */
+};
+
+export default MainSection;
