8c8ad7fb87f7b7499fcb268f8a2ce7f02f99508f
rtkt <yetanotherinbox@list.ru>
2018-01-04 17:00:47 +0700

    Rewrote App component to use MainSection instead of ExploreSection and deleted ExploreSection from project

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import ExploreSection from '../ExploreSection/';
+import styled from 'styled-components';
+import MainSection from '../MainSection/';
 import Header from '../Header/';
+import HorizontalCard from '../HorizontalCard/';
+import exploreSectionPhoto1 from '../imgs/exploreSection-1.png';
+import exploreSectionPhoto2 from '../imgs/exploreSection-2.png';
+import exploreSectionPhoto3 from '../imgs/exploreSection-3.png';
+
+const Main = styled.main`
+  margin-top: 128px;
+`;
 
 const App = function () {
-  return ([
+  return [
     <Header />,
-    <main>
+    <Main>
       <div className="container">
-        <ExploreSection heading="Explore Airbnb" />
+        <MainSection heading="Explore Airbnb">
+          <div className="row">
+            <div className="col-lg-4">
+              <HorizontalCard img={exploreSectionPhoto1} text="Homes" />
+            </div>
+            <div className="col-lg-4">
+              <HorizontalCard img={exploreSectionPhoto2} text="Experiences" />
+            </div>
+            <div className="col-lg-4">
+              <HorizontalCard img={exploreSectionPhoto3} text="Restaurants" />
+            </div>
+          </div>
+        </MainSection>
       </div>
-    </main>,
-  ]);
+    </Main>,
+  ];
 };
 
 export default App;
