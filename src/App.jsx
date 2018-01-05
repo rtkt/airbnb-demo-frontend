49b1b360c09ed7e3e8c2f12cf0f6b98c1b729cce
rtkt <yetanotherinbox@list.ru>
2018-01-06 04:18:51 +0700

    Moved everything from Main.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import Experiences from './Experiences';
+import Explore from './Explore';
 import Header from './Header';
-import Main from './Main';
+import Homes from './Homes';
+import Popular from './Popular';
+
+const Main = styled.main`
+  margin-top: 128px;
+`;
 
 const App = function app() {
   return (
     <React.Fragment>
       <Header key="header" />
-      <Main key="main" />
+      <Main key="main">
+        <div className="container">
+          <Explore />
+          <Experiences />
+          <Homes />
+          <Popular />
+        </div>
+      </Main>
     </React.Fragment>
   );
 };
