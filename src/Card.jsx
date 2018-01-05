9cbcd3db6249785256f25b19cadbeb455fb606c4
rtkt <yetanotherinbox@list.ru>
2018-01-05 15:15:15 +0700

    Moved all common elements from PlaceCard.jsx to Card.jsx

---
+++ src/Card.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+`;
+
+export const FlexLine = styled.span`
+  align-items: baseline;
+  display: flex;
+`;
+
+export const Price = styled.span`
+  font-weight: bold;
+  &::before {
+    content: '$';
+  }
+`;
+
+export const Reviews = FlexLine.extend`
+  font-size: 12px;
+`;
+
+export const RightToStars = styled.span`
+  color: #383838;
+  margin-left: 8px;
+`;
+
+export const Star = styled.img`
+  margin-right: 4px;
+`;
+
+export const VerticalContainer = Container.extend`
+  align-items: flex-start;
+  flex-direction: column;
+`;
--- src/PlaceCard.jsx
+++ src/PlaceCard.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Price, Reviews, RightToStars, Star, VerticalContainer } from './Card';
 import star from './imgs/star.svg';
-
-const Container = styled.div`
-  align-items: flex-start;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
-`;
 
 const Name = styled.span`
   font-size: 15px;
@@ -16,30 +10,9 @@ const Name = styled.span`
   margin-top: 8px;
 `;
 
-const Price = styled.span`
-  font-weight: bold;
-  &::before {
-    content: '$';
-  }
-`;
-
-const Reviews = styled.span`
-  align-items: baseline;
-  display: flex;
-  font-size: 12px;
-`;
-
-const ReviewsCounter = styled.span`
-  margin-left: 8px;
-`;
-
-const Star = styled.img`
-  margin-right: 4px;
-`;
-
 const PlaceCard = function placeCard(props) {
   return (
-    <Container>
+    <VerticalContainer>
       <img src={props.img} alt={props.alt} title={props.alt} />
       <Name>
         <Price>{props.price} </Price>
@@ -51,9 +24,9 @@ const PlaceCard = function placeCard(props) {
         <Star src={star} />
         <Star src={star} />
         <Star src={star} />
-        <ReviewsCounter>{props.numOfReviews}</ReviewsCounter>
+        <RightToStars>{props.numOfReviews}</RightToStars>
       </Reviews>
-    </Container>
+    </VerticalContainer>
   );
 };
 
