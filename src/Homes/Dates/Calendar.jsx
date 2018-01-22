552f09a2b942e5ef4c4b4da6d356815aa3261c75
rtkt <yetanotherinbox@list.ru>
2018-01-23 02:55:12 +0700

    Added md+ calendar styling

--- src/Homes/Dates/Calendar.jsx
+++ src/Homes/Dates/Calendar.jsx
@@ -13,10 +13,14 @@ const Wrapper = styled.div`
   box-sizing: border-box;
   box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
   border-radius: 4px;
-  left: 81px;
+  left: 8px;
   position: fixed;
   top: 132px;
   z-index: 20;
+
+  @media(min-width: ${breakpoints.lg}) {
+    left: 81px;
+  }
 `;
 
 const getMonthsNumber = () => {
